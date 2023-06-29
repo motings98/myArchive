c1493aceba0e73dd686c1696c13474782573bae8
Colder <535602640@qq.com>
2023-06-29 17:24:52 +0800

    增加nivo-lightbox 图片展示

--- js/main.js
+++ js/main.js
@@ -97,6 +97,39 @@ $(function(){
 		);
 	}
 
+	// Portfolio isotope filter
+    $(window).on('load', function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
 
+    });
+	
+
+    // Nivo Lightbox 
+    $('.portfolio-item a').nivoLightbox({
+            effect: 'slideDown',  
+            keyboardNav: true,                            
+        });
 
 });
